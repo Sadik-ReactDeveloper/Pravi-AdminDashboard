19e5973d88af97328b6a1a78bcd1e28e6007ca9e
anujeshyadav <anujeshdevelopersveltosest11@gmail.com>
2023-09-05 16:38:22 +0530

    build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9cc23d99164a0090feee776d60b6d49c.js"
+  "/precache-manifest.b5446a2e831eb55ff0eef734af369f0c.js"
 );
 
 self.addEventListener('message', (event) => {
---
+++ build/static/js/312.f85246be.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[312],{2454:function(a,s){}}]);
+//# sourceMappingURL=312.f85246be.chunk.js.map
